b60b3d41daa3987c3346169c18745f22493d3adf
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2024-04-09 22:38:36 +0100

    mobile responseive map

--- src/components/IssueForm.js
+++ src/components/IssueForm.js
@@ -6,7 +6,7 @@ import LeafletMap from './LeafletMap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
 
 const IssueForm = ({ category }) => {
 
@@ -21,7 +21,7 @@ const IssueForm = ({ category }) => {
         email: "",
         address: "",
         map: "",
-        housingtype: ""
+        housingType: ""
     })
 
     const [selectedFile, setSelectedFile] = useState(null);
@@ -93,88 +93,87 @@ const IssueForm = ({ category }) => {
 
             <div className="content-wrapper">
 
-                {showForm &&
-                    <div id="form-container">
-                        <div className="close-button" onClick={() => setShowForm(false)}>                <FontAwesomeIcon icon={faBars} style={{ fontSize: '24px' }} />
-                        </div>
+                <div id="form-container">
+                    {/* <div className="close-button"><FontAwesomeIcon icon={faBackward} style={{ fontSize: '24px' }} />Back</div> */}
 
-                        <p id='info'>Please select the location on the map and provide details about the issue.</p>
+                    <p id='info'>Please select the location on the map and provide details about the issue.</p>
 
-                        {showAlert &&
-                            <Alert variant="dark" onClose={() => setShowAlert(false)} dismissible>
-                                Your report has been sent to Cllr James McPhilemy. Thank you for your help!
-                            </Alert>
+                    <div id="map-container">
+                        <LeafletMap formData={formData} setFormData={setFormData} setShowForm={setShowForm} showForm={showForm} />
+                    </div>
+
+                    {showAlert &&
+                        <Alert variant="dark" onClose={() => setShowAlert(false)} dismissible>
+                            Your report has been sent to Cllr James McPhilemy. Thank you for your help!
+                        </Alert>
+                    }
+
+                    <Form onSubmit={onSubmit} id="issue-form">
+                        <Form.Group>
+                            <Form.Control
+                                onChange={onChange}
+                                type="text"
+                                id="name"
+                                name="name"
+                                required
+                                placeholder="Your Name"
+                                value={formData.name}
+                            />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Control
+                                onChange={onChange}
+                                type="email"
+                                id="email"
+                                placeholder="Your Email"
+                                name="email"
+                                required
+                                value={formData.email}
+                            />
+                        </Form.Group>
+
+                        {category === 'housing' &&
+                            <Form.Group>
+                                <Form.Select
+                                    onChange={onChange} // Add the onChange function here
+                                    id="housingType" // Add an id to identify the select element
+                                    name="housingType" // Add a name to identify the select element
+                                    value={formData.housingType} // Set the value to the corresponding state value
+                                >
+                                    <option>Choose type of tenancy</option>
+                                    <option value="1">Council</option>
+                                    <option value="2">Housing Association</option>
+                                    <option value="3">Private</option>
+                                </Form.Select>
+                            </Form.Group>
                         }
 
-                        <Form onSubmit={onSubmit} id="issue-form">
-                            <Form.Group>
-                                <Form.Control
-                                    onChange={onChange}
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    required
-                                    placeholder="Your Name"
-                                    value={formData.name}
-                                />
-                            </Form.Group>
+                        <Form.Group>
+                            <Form.Control
+                                as="textarea"
+                                onChange={onChange}
+                                id="description"
+                                name="description"
+                                value={formData.description}
+                                placeholder="Describe the issue..."
+                                rows="4"
+                                required
+                            />
+                        </Form.Group>
 
-                            <Form.Group>
-                                <Form.Control
-                                    onChange={onChange}
-                                    type="email"
-                                    id="email"
-                                    placeholder="Your Email"
-                                    name="email"
-                                    required
-                                    value={formData.email}
-                                />
-                            </Form.Group>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Control type="file" onChange={handleFileChange} />
+                        </Form.Group>
 
-                            {category === 'housing' &&
-                                <Form.Group>
-                                    <Form.Select
-                                        onChange={onChange} // Add the onChange function here
-                                        id="tenancyType" // Add an id to identify the select element
-                                        name="tenancyType" // Add a name to identify the select element
-                                        value={formData.tenancyType} // Set the value to the corresponding state value
-                                    >
-                                        <option>Choose type of tenancy</option>
-                                        <option value="1">Council</option>
-                                        <option value="2">Housing Association</option>
-                                        <option value="3">Private</option>
-                                    </Form.Select>
-                                </Form.Group>
-                            }
+                        <div id="buttons-container">
+                            <Button type="submit" id="save" className="formheader send-button">{plane} <span className="send-text">Send</span></Button>
+                            <Button id="cancel" className="send-button" onClick={() => navigate('/')}>Cancel</Button>
+                        </div>
+                    </Form>
 
-                            <Form.Group>
-                                <Form.Control
-                                    as="textarea"
-                                    onChange={onChange}
-                                    id="description"
-                                    name="description"
-                                    value={formData.description}
-                                    placeholder="Describe the issue..."
-                                    rows="4"
-                                    required
-                                />
-                            </Form.Group>
-
-                            <Form.Group controlId="formFile" className="mb-3">
-                                <Form.Control type="file" onChange={handleFileChange} />
-                            </Form.Group>
-
-                            <div id="buttons-container">
-                                <Button type="submit" id="save" className="formheader send-button">{plane} <span className="send-text">Send</span></Button>
-                                <Button id="cancel" className="send-button" onClick={() => navigate('/')}>Cancel</Button>
-                            </div>
-                        </Form>
-
-                    </div>
-                }
-                <div id="map-container">
-                    <LeafletMap formData={formData} setFormData={setFormData} setShowForm={setShowForm} showForm={showForm} />
                 </div>
+
             </div>
         </>
 
